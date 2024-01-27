e72a99308506e30b4e9c96a8aaa28bce46932e3a
prasanth4a5 <ummadi1994@gmail.com>
2024-01-27 18:45:25 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
-output "subnets" {
-  value = module.subnets
-}
 
-output "public_subnet_ids" {
-  value = local.public_subnet_ids
-}
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+
+#output "subnets" {
+#  value = module.subnets
+#}
+#
+#output "public_subnet_ids" {
+#  value = local.public_subnet_ids
+#}
+#
+#output "vpc_id" {
+#  value = aws_vpc.main.id
+#}
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,7 +1,17 @@
-output "subnet_ids" {
-  value = aws_subnet.main
-}
 
-output "route_table_ids" {
-  value = aws_route_table.main
-}
+
+
+
+
+
+
+
+
+
+#output "subnet_ids" {
+#  value = aws_subnet.main
+#}
+#
+#output "route_table_ids" {
+#  value = aws_route_table.main
+#}
