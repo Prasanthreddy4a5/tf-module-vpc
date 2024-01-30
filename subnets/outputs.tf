6c805b9eb0d0a0a03736e3e710964e7b4036cf31
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-01-30 18:06:34 +0530

    test

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,17 +1,14 @@
-output "Subnet_id" {
-  value = lookup(lookup(aws_subnet.main, each.key, null), "id", null)
+#
+
+
+
+
+
+
+output "subnet_ids" {
+  value = aws_subnet.main
 }
 
-
-
-
-
-
-
-#output "subnet_ids" {
-#  value = aws_subnet.main
-#}
-#
-#output "route_table_ids" {
-#  value = aws_route_table.main
-#}
+output "route_table_ids" {
+  value = aws_route_table.main
+}
