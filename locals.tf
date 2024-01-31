39dbffb6ca2f0e3397a86b50ecf5f71e8523e90a
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-01-31 22:34:05 +0530

    test

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 #
 locals {
-
   # Subnets
   public_subnet_ids  = [for k, v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null) : v.id]
   app_subnet_ids     = [for k, v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null) : v.id]
