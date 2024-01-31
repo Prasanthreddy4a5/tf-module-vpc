5bb17305c49ef917bb3320c7e313e006614b4458
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-01-31 16:28:58 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -5,9 +5,9 @@ output "subnets" {
   value = module.subnets
 }
 
-output "natgateway" {
-  value = aws_nat_gateway.ngw.allocation_id
-}
+#output "natgateway" {
+#  value = aws_nat_gateway.ngw
+#}
 
 #output "public_subnet_ids" {
 #  value = local.public_subnet_ids
