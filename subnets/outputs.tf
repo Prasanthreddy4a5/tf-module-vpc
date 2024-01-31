50c2c89ea49987735a79dab03388c824753d8fef
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-01-31 23:04:54 +0530

    test

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,8 +1,8 @@
 #
 output "subnet_ids" {
-  value = aws_subnet.main
+  value = aws_subnet.main.id
 }
 
 output "route_table_ids" {
-  value = aws_route_table.main
+  value = aws_route_table.main.id
 }
