2e0e4d0383d207080ec95f0be7bdcd3d87fd6d54
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-01-31 23:22:31 +0530

    test

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
-  ip_protocol       = "ssh"
+  ip_protocol       = "tcp"
   to_port           = 22
 }
 
