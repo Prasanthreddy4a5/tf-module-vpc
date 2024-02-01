f2b7d9da6d86866ee29681e118e87f0bfcde944a
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-01 11:56:01 +0530

    test-tags

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
-  tags       = {
-    Name = "Created By Terraform"
-  }
+  tags       = merge(local.tags, {Name = "${var.env}-vpc"}
+
   }
 module "subnets" {
     source   = "./subnets"
     for_each = var.subnets
     subnets  = each.value
     vpc_id   = aws_vpc.main.id
-
+    tags     = var.tags
   }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "MAINPUBLIC"
-  }
+  tags = merge(local.tags, {Name = "${var.env}-ig"}
 
 }
 
@@ -50,6 +47,7 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
+      tags = merge(local.tags, {Name = "${var.env}-peer"}
 }
 
 resource "aws_route" "peer" {
@@ -65,37 +63,39 @@ resource "aws_route" "default-vpc-peer-entry" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id = local.app_subnet_ids[0]
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_all"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
 
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+#resource "aws_instance" "test" {
+#  ami           = "ami-0f3c7d07486cad139"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  subnet_id = local.app_subnet_ids[0]
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic and all outbound traffic"
+#  vpc_id      = aws_vpc.main.id
+#
+#  tags = {
+#    Name = "allow_all"
+#  }
+#}
+#
+#resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#  security_group_id = aws_security_group.allow_tls.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  from_port         = 22
+#  ip_protocol       = "tcp"
+#  to_port           = 22
+#}
+#
+#
+#resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#  security_group_id = aws_security_group.allow_tls.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  ip_protocol       = "-1" # semantically equivalent to all ports
+#}
 
 
 
