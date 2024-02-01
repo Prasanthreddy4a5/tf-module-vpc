d03ec646f8fc28f76ece9cff52d73b01b4c26703
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-01 10:59:07 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -11,18 +11,5 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-#output "subnets" {
-#  value = module.subnets
-#}
 
-#output "natgateway" {
-#  value = aws_nat_gateway.ngw
-#}
 
-#output "public_subnet_ids" {
-#  value = local.public_subnet_ids
-#}
-#
-#output "vpc_id" {
-#  value = aws_vpc.main.id
-#}
